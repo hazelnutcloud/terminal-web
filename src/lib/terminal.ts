b785f3c2d0460ed81569f6a002bbd1923c55c0e3
hazelnutcloud <hsnmkls@proton.me>
2024-12-09 23:37:32 +0800

    terminal

---
+++ src/lib/terminal.ts
@@ -0,0 +1,129 @@
+import { Terminal } from "@xterm/xterm";
+import { WebglAddon } from "@xterm/addon-webgl"
+import { FitAddon } from "@xterm/addon-fit";
+import chalk from "chalk";
+import type { Action } from "svelte/action";
+import { command as brocli, run, type BroCliConfig } from "@drizzle-team/brocli"
+
+const theme = {
+  foreground: '#a6accd',
+  background: '#1b1e28',
+  selection: '#a6accd',
+  black: '#1b1e28',
+  brightBlack: '#a6accd',
+  red: '#d0679d',
+  brightRed: '#d0679d',
+  green: '#5de4c7',
+  brightGreen: '#5de4c7',
+  yellow: '#fffac2',
+  brightYellow: '#fffac2',
+  blue: '#89ddff',
+  brightBlue: '#add7ff',
+  magenta: '#fcc5e9',
+  brightMagenta: '#fae4fc',
+  cyan: '#add7ff',
+  brightCyan: '#89ddff',
+  white: '#ffffff',
+  brightWhite: '#ffffff'
+}
+
+export const terminal: Action = (node) => {
+  const term = new Terminal({
+    cursorBlink: true,
+    fontFamily: '"Kode Mono", monospace',
+    theme,
+    convertEol: true
+  })
+
+  const originalLog = console.log
+  const originalError = console.error
+  console.log = console.error = (...args: unknown[]) => {
+    term.write(args.join(" "))
+  }
+
+  const commands = [
+    brocli({
+      name: "connect",
+      handler: () => {
+        console.log("Connecting Wallet")
+      },
+
+    })
+  ]
+
+  const prompt = () => {
+    term.writeln(chalk.blue("\r\nanon"))
+    term.write(chalk.magenta("> "))
+  }
+
+  const runCommand = async () => {
+    const args = command.trim().split(' ');
+    if (args[0]?.length > 0) {
+      term.writeln('')
+      await run(commands, {
+        argSource: ["", "", ...args],
+        noExit: true
+      } as BroCliConfig)
+    }
+    term.writeln('')
+    prompt()
+  }
+
+  const fitAddon = new FitAddon()
+
+  term.loadAddon(new WebglAddon())
+  term.loadAddon(fitAddon)
+
+  term.open(node)
+  fitAddon.fit();
+
+  term.writeln(chalk.blue("                ___                           "))
+  term.writeln(chalk.blue("               /\\_ \\    __                    "))
+  term.writeln(chalk.blue(" _____   __  __\\//\\ \\  /\\_\\  ____      __     "))
+  term.writeln(chalk.blue("/\\ '__`\\/\\ \\/\\ \\ \\ \\ \\ \\/\\ \\/\\_ ,`\\  /'__`\\   "))
+  term.writeln(chalk.blue("\\ \\ \\L\\ \\ \\ \\_\\ \\ \\_\\ \\_\\ \\ \\/_/  /_/\\ \\L\\.\\_ "))
+  term.writeln(chalk.blue(" \\ \\ ,__/\\/`____ \\/\\____\\\\ \\_\\/\\____\\ \\__/.\\_\\"))
+  term.writeln(chalk.blue("  \\ \\ \\/  `/___/> \\/____/ \\/_/\\/____/\\/__/\\/_/"))
+  term.writeln(chalk.blue("   \\ \\_\\     /\\___/                           "))
+  term.writeln(chalk.blue("    \\/_/     \\/__/                            "))
+  term.writeln("")
+  term.writeln(chalk.green("enter `--help` to get started."))
+
+  prompt()
+
+  let command = ''
+
+  term.onData(e => {
+    switch (e) {
+      case '\u0003': // Ctrl+C
+        term.writeln('^C');
+        prompt();
+        break;
+      case '\r': // Enter
+        runCommand();
+        command = '';
+        break;
+      case '\u007F': // Backspace (DEL)
+        // Do not delete the prompt
+        if (term.buffer.active.cursorX > 2) {
+          term.write('\b \b');
+          if (command.length > 0) {
+            command = command.slice(0, command.length - 1);
+          }
+        }
+        break;
+      default: // Print all other characters for demo
+        if (e >= String.fromCharCode(0x20) && e <= String.fromCharCode(0x7E) || e >= '\u00a0') {
+          command += e;
+          term.write(e);
+        }
+    }
+  });
+
+  return {
+    destroy() {
+      console.log = originalLog
+      console.error = originalError
+    },
+  }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import '@xterm/xterm/css/xterm.css';
+	import { terminal } from '$lib/terminal';
+</script>
+
+<div class="h-svh w-full py-4 px-8 bg-[#1b1e28]">
+	<div class="h-full w-full" use:terminal></div>
+</div>
